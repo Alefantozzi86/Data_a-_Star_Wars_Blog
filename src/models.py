5f96b2a88067dbd38d89b5f4ad7a5a51a04306e4
Alefantozzi86 <alefantozzi86@gmail.com>
2023-04-01 22:50:39 +0000

    my-diagram

--- src/models.py
+++ src/models.py
@@ -6,28 +6,57 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+   
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(15), nullable=False)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name_character = Column(String(50), nullable=False, unique=True)
+    gender = Column(String(10))
+    height = Column(Integer)
+    hair_color = Column(String(50))
+    skin_color = Column(String(50))
+    eye_color = Column(String(50))
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+       
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name_planets = Column(String(50), nullable=False, unique=True)
+    climate = Column(String(50))
+    population = Column(Integer)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
 
-    def to_dict(self):
-        return {}
+class Films(Base):
+    __tablename__ = 'films'
+    id =  Column(Integer, primary_key=True)
+    title = Column(String(50))
+    episode_id = Column(String(50))
+    director = Column(String(50))
+    producer = Column(String(50))
+    release_date = Column(Integer)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable= False)
+    
+    
+    
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
